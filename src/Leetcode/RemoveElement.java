1200b788752298fa4e48413957685a95ca31eaac
Aryan Upadhyay <aryan.offc@gmail.com>
2022-08-05 14:18:58 +0530

    Remove Element.

---
+++ src/Leetcode/RemoveElement.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class RemoveElement {
+
+    public int removeElement(int[] arr, int val) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != val) {
+                arr[count++] = arr[i];
+            }
+        }
+
+        return count;
+    }
+
+}
