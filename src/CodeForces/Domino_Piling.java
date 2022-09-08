e37e47e4725a2b43b79e87e3d559df4f7fce2451
Aryan Upadhyay <aryan.offc@gmail.com>
2022-09-08 22:39:14 +0530

    Domino_Piling

---
+++ src/CodeForces/Domino_Piling.java
@@ -0,0 +1,57 @@
+package CodeForces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Domino_Piling {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] a) {
+        FastReader sc = new FastReader();
+        int n = sc.nextInt(), m = sc.nextInt();
+        System.out.println(n*m/2);
+    }
+
+}
