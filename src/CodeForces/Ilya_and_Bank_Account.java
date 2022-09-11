4dcd47bbb9b2522c499db18cf1934690e55cc003
Aryan Upadhyay <aryan.offc@gmail.com>
2022-09-11 10:44:18 +0530

    Ilya_and_Bank_Account

---
+++ src/CodeForces/Ilya_and_Bank_Account.java
@@ -0,0 +1,62 @@
+package CodeForces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.lang.*;
+
+public class Ilya_and_Bank_Account {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        int amount = sc.nextInt();
+
+        if (amount > 0)
+            System.out.println(amount);
+        else {
+            int val1 = amount;
+        }
+    }
+}
